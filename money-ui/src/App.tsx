edeaea6dff0c82880307aaea9a38fb2edeb23838
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2023-07-01 21:20:24 +0300

    remove unnecessary code; add custom logo

--- money-ui/src/App.tsx
+++ money-ui/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './money-svgrepo-com.svg'
 
 function App() {
   return (
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Web Application for money tracking.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
