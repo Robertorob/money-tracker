4680180b1f81d48902081d9fdea212227d4e8bc6
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2023-07-04 19:49:08 +0300

    extract tab-panel

--- money-ui/src/App.tsx
+++ money-ui/src/App.tsx
@@ -1,36 +1,10 @@
 import React from 'react';
 import './App.css';
-import HelloPage from './components/hello-page'
+import HelloPage from './components/hello-page/hello-page'
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+import TabPanel from './components/tab/tab-panel';
 
 function a11yProps(index: number) {
   return {
