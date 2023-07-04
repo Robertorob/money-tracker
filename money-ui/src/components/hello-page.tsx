f43ec508e02ba7d642fec2f94f13c57c4f1f1140
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2023-07-04 19:41:06 +0300

    remove unnecessary css classes

--- money-ui/src/components/hello-page.tsx
+++ money-ui/src/components/hello-page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
+import './hello-page.css';
 
 const HelloPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="hello-page">
+      <header className="hello-page-header">
+        <img src={logo} className="hello-page-logo" alt="logo" />
         <p>
           Web Application for money tracking.
         </p>
