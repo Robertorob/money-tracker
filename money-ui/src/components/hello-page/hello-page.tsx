91d1a7c715485a68fbb44afac24333fe473c99dd
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2023-08-29 23:41:37 +0300

    fix envs and docker compose

--- money-ui/src/components/hello-page/hello-page.tsx
+++ money-ui/src/components/hello-page/hello-page.tsx
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './hello-page.css';
 
 const HelloPage = () => {
+
+
   return (
     <div className="hello-page">
       <header className="hello-page-header">
         <img src={logo} className="hello-page-logo" alt="logo" />
         <p>
           Web Application for money tracking.
+          <br/>
+          Server URL: {process.env.REACT_APP_SERVER_URL}
+          <br/>
+          Env: {process.env.NODE_ENV}
         </p>
       </header>
     </div>)
