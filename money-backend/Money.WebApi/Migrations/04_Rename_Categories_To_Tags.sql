4f7fac953e8d1ec34107d637db403c7e0c4d3acc
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2024-02-12 23:31:26 +0300

    add migration to rename "category" to "tag"

---
+++ money-backend/Money.WebApi/Migrations/04_Rename_Categories_To_Tags.sql
@@ -0,0 +1,10 @@
+﻿ALTER TABLE spendings
+    DROP CONSTRAINT fk_spendings_categories;
+
+ALTER TABLE spendings 
+RENAME category_id TO tag_id;
+
+ALTER TABLE categories RENAME TO tags;
+
+ALTER TABLE spendings
+    ADD CONSTRAINT fk_spendings_tags FOREIGN KEY (tag_id) REFERENCES tags (id);
