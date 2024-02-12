efbf776fe62f209ed67d82d1d69395141a458e49
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2024-02-12 23:40:30 +0300

    fix renaming

--- money-backend/Money.BusinessLogic/Dto/Tag/CreateCategoryResultDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Money.BusinessLogic.Dto.Category;
-
-public class CreateCategoryResultDto
-{
-  public long Id { get; set; }
-
-  public string Name { get; set; }
-}
---
+++ money-backend/Money.BusinessLogic/Dto/Tag/CreateTagResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Money.BusinessLogic.Dto.Tag;
+
+public class CreateTagResultDto
+{
+  public long Id { get; set; }
+
+  public string Name { get; set; }
+}
