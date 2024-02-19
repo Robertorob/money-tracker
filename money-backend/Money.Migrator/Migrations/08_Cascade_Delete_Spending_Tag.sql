383d2dfd7cf44803ce03cf1b7eb3e575aaac3544
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2024-02-19 22:03:52 +0300

    add web-api migrations in gitignore; delete relational record on spending-deleting

---
+++ money-backend/Money.Migrator/Migrations/08_Cascade_Delete_Spending_Tag.sql
@@ -0,0 +1,3 @@
+﻿alter table spending_tag
+drop constraint fk_spendings_tags_spending,
+add constraint fk_spendings_tags_spending foreign key (spendings_id) references spendings (id) on delete cascade;
