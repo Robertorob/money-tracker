369bbe4c01e6ccf6650cc2a2a9487d53e17c9ac0
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2024-10-27 18:04:11 +0300

    remove unnecessary dependencies from ManagementController

--- money-backend/Money.WebApi/Controllers/ManagementController.cs
+++ money-backend/Money.WebApi/Controllers/ManagementController.cs
@@ -1,19 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using Money.BusinessLogic.Interfaces;
 
 namespace Money.WebApi.Controllers;
 [ApiController]
 public class ManagementController : ControllerBase
 {
-    private readonly ILogger<SpendingsController> _logger;
-    private readonly ISpendingsService _spendingService;
-
-    public ManagementController(ILogger<SpendingsController> logger, ISpendingsService spendingService)
-    {
-        _logger = logger;
-        _spendingService = spendingService;
-    }
-
     [HttpGet]
     [Route("ping")]
     public string IsAlive()
