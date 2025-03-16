6c23c444b96ee4cf88f2cade41c8bf500a6559a3
Ахметшин Роберт Данилович <RDAkhmetshin@alfabank.ru>
2025-03-16 17:57:25 +0300

    MMBLT-726: добавить drawer

--- money-ui/src/App.tsx
+++ money-ui/src/App.tsx
@@ -5,14 +5,67 @@ import TabContainer from './components/tab/tab-container';
 import Contacts from './components/contacts/contacts';
 import Spendings from './components/spendings/spendings';
 import Tags from './components/tags/tags';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 export default function App() {
+
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Счётчик дней', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton onClick={() => alert('реализовать страницу')}>
+              <ListItemIcon>
+                {index == 0 ? <CalendarMonthIcon /> : null}
+                {index !== 0 ? <InboxIcon /> : null}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <TabContainer labels={['Home', 'Spendings', 'Tags', 'Contacts']}>
-      <HelloPage />
-      <Spendings />
-      <Tags />
-      <Contacts />
-    </TabContainer>
+    <>
+      <Button onClick={toggleDrawer(true)} >
+        <MenuIcon fontSize='large' color='primary' />
+      </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+      <TabContainer labels={['Home', 'Spendings', 'Tags', 'Contacts']}>
+        <HelloPage />
+        <Spendings />
+        <Tags />
+        <Contacts />
+      </TabContainer>
+    </>
   );
 }
